f7864b9770512917c5415081b8b065a716232f75
Sivaranjani-A <112957992+Sivaranjani-A@users.noreply.github.com>
2022-11-26 07:35:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,80 +2,155 @@ import logo from './logo.svg';
 import './App.css';
 
 
+
 function App() {
-  const contentsdata = [{
-    h5: "free",
-    h2: "$0",
-    l1: "Single User",
-    l2: "5GB Storage",
-    l3: "Unlimited Public Projects",
-    l4: "Community Access.",
-    l5: <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Unlimited
-      Private Projects</li>,
-    l6: <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Dedicated
-      Phone Support</li>,
-    l7: <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Free Subdomain
-    </li>,
-    l8: <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Monthly Status
-      Reports</li>
+  const data = [{
+    plan: "free",
+    price: "$0",
+    features: [
+      {
+        title: "Single User",
+        correct: true
+      },
+      {
+        title: "5GB Storage",
+        correct: true
+      },
+      {
+        title: "Unlimited Public Projects",
+        correct: true
+      },
+      {
+        title: "Community Access",
+        correct: true
+      },
+      {
+        title: "Unlimited Private Projects",
+        correct: false
+      },
+      {
+        title: "Dedicated Phone Support",
+        correct: false
+      },
+      {
+        title: "Free Subdomain",
+        correct: false
+      },
+      {
+        title: "Monthly Status Reports",
+        correct: false
+      }
+    ]
   },
   {
-    h5: "plus",
-    h2: "$9",
-    l1: <strong>5 Users</strong>,
-    l2: "50GB Storage",
-    l3: "Unlimited Public Projects",
-    l4: "Community Access",
-    l5: <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Private Projects</li>,
-    l6: <li><span class="fa-li"><i class="fas fa-check"></i></span>Dedicated Phone Support</li>,
-    l7: <li><span class="fa-li"><i class="fas fa-check"></i></span>Free Subdomain</li>,
-    l8: <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Monthly Status
-      Reports</li>
+    plan: "PLUS",
+    price: "$9",
+    features: [
+      {
+        title: "5 Users",
+        correct: true,
+        highlight: true
+      },
+      {
+        title: "50GB Storage",
+        correct: true
+      },
+      {
+        title: "Unlimited Public Projects",
+        correct: true
+      },
+      {
+        title: "Community Access",
+        correct: true
+      },
+      {
+        title: "Unlimited Private Projects",
+        correct: true
+      },
+      {
+        title: "Dedicated Phone Support",
+        correct: true
+      },
+      {
+        title: "Free Subdomain",
+        correct: true
+      },
+      {
+        title: "Monthly Status Reports",
+        correct: false
+      }
+    ]
   },
   {
-    h5: "pro",
-    h2: "$49",
-    l1: <strong>Unlimited Users</strong>,
-    l2: "150GB Storage",
-    l3: "Unlimited Public Projects",
-    l4: "Community Access",
-    l5: <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Private Projects</li>,
-    l6: <li><span class="fa-li"><i class="fas fa-check"></i></span>Dedicated Phone Support</li>,
-    l7: <li><span class="fa-li"><i class="fas fa-check"></i></span><strong>Unlimited</strong> Free
-      Subdomains</li>,
-    l8: <li><span class="fa-li"><i class="fas fa-check"></i></span>Monthly Status Reports</li>
+    plan: "PRO",
+    price: "$49",
+    features: [
+      {
+        title: "Unlimited Users",
+        correct: true,
+        highlight: true
+      },
+      {
+        title: "150GB Storage",
+        correct: true
+      },
+      {
+        title: "Unlimited Public Projects",
+        correct: true
+      },
+      {
+        title: "Community Access",
+        correct: true
+      },
+      {
+        title: "Unlimited Private Projects",
+        correct: true
+      },
+      {
+        title: "Dedicated Phone Support",
+        correct: true
+      },
+      {
+        title: "Unlimited Free Subdomains",
+        correct: true
+      },
+      {
+        title: "Monthly Status Reports",
+        correct: true
+      }
+    ]
   }
-  ]
+  ];
 
   return (
     <div className="App">
       <section class="pricing py-5">
         <div class="container">
           <div class="row">
-            {contentsdata.map((ele) => <Card h5={ele.h5} h2={ele.h2} l1={ele.l1} l2={ele.l2} l3={ele.l3} l4={ele.l4} l5={ele.l5} l6={ele.l6} l7={ele.l7} l8={ele.l8} />)}
+            {data.map((ele) => {
+              return <Card card={ele} />
+            })}
+
           </div>
         </div>
       </section>
     </div>
   );
 }
-function Card({ h5, h2, l1, l2, l3, l4, l5, l6, l7, l8 }) {
+function Card({ card }) {
   return (
     <div class="col-lg-4">
       <div class="card mb-5 mb-lg-0">
         <div class="card-body">
-          <h5 class="card-title text-muted text-uppercase text-center">{h5}</h5>
-          <h6 class="card-price text-center">{h2}<span class="period">/month</span></h6>
+          <h5 class="card-title text-muted text-uppercase text-center">{card.plan}</h5>
+          <h6 class="card-price text-center">{card.price}<span class="period">/month</span></h6>
           <hr />
           <ul class="fa-ul">
-            <li><span class="fa-li"><i class="fas fa-check"></i></span>{l1}</li>
-            <li><span class="fa-li"><i class="fas fa-check"></i></span>{l2}</li>
-            <li><span class="fa-li"><i class="fas fa-check"></i></span>{l3}</li>
-            <li><span class="fa-li"><i class="fas fa-check"></i></span>{l4}</li>
-            {l5}
-            {l6}
-            {l7}
-            {l8}
+            {card.features.map((item) => {
+
+              return <li class={item.correct ? "" : "text-muted"}><span class="fa-li"><i class={item.correct ? "fas fa-check" : "fas fa-times"}></i></span>{item.highlight ? <strong>{item.title}</strong> : <span>{item.title}</span>}</li>
+            }
+            )}
           </ul>
           <div class="d-grid">
             <a href="#" class="btn btn-primary text-uppercase">Button</a>
@@ -87,3 +162,4 @@ function Card({ h5, h2, l1, l2, l3, l4, l5, l6, l7, l8 }) {
 }
 
 export default App;
+
